8223f87e792c993168cd4b5d10c1c78c000a76a7
Miguel Collado <m.collado.gomez@gmail.com>
2021-05-15 18:04:50 +0200

    Actualizar rebalance.sh

--- bin/rebalance.sh
+++ bin/rebalance.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR/..
+cd $SCRIPT_DIR
+cd ..
+
+source ~/.bashrc
 
 # environment variables
 source .environment
