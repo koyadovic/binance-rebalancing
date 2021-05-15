05825d37f5d68e8580e6d2e1c120c191fde76d0d
Miguel Collado <m.collado.gomez@gmail.com>
2021-05-15 19:18:35 +0200

    Actualizar rebalance.sh

--- bin/rebalance.sh
+++ bin/rebalance.sh
@@ -4,8 +4,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 cd ..
 
+source ~/.bash_profile
+source ~/.bashrc
+
 # environment variables
 source .environment
 
 # execute it
-python rebalance.py --yes
+python rebalance.py --yes &> /tmp/rebalance.output
+date >> /tmp/rebalance.output
