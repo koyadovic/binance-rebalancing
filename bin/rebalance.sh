04627dafb92c59c4d60456d209c33e326feb7ea2
Miguel Collado <m.collado.gomez@gmail.com>
2021-05-16 10:55:21 +0200

    Actualizar rebalance.sh

--- bin/rebalance.sh
+++ bin/rebalance.sh
@@ -4,6 +4,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 cd ..
 
+wait_seconds=$((120 + $RANDOM % 120))
+echo Waiting aleatory $wait_seconds seconds ...
+sleep $wait_seconds
+
 source ~/.bash_profile
 source ~/.bashrc
 
