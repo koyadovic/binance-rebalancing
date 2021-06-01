e0b42e4379fdcfe1ed2dc7a81440b356ee756751
Miguel Collado <m.collado.gomez@gmail.com>
2021-06-01 11:54:32 +0200

    Actualizar settings.py

--- settings.py
+++ settings.py
@@ -4,14 +4,6 @@ import os
 API_KEY = os.environ.get('BINANCE_API_KEY')
 API_SECRET = os.environ.get('BINANCE_API_SECRET')
 
-
-"""
-Fiat asset selected. Use BUSD, USDT, and so on
-"""
-
-fiat_asset = 'BUSD'
-fiat_decimals = 2
-
 """
 Crypto: Percentage
 """
@@ -40,4 +32,13 @@ portfolio_setting = {
 
 
 # Percentage of deviation to rebalance. Set to None if want to disable it
-minimum_percentage_deviation = 1.0  # for 1.0 value if asset has 10%, causes rebalancing if it takes >=11% or <=9%
+# for 1.0 value if asset has 10%, causes rebalancing if it takes >=11% or <=9%
+minimum_percentage_deviation = 1.0 * exposure
+
+
+"""
+Fiat asset selected. Use BUSD, USDT, and so on
+"""
+
+fiat_asset = 'BUSD'
+fiat_decimals = 2
