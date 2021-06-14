ffae46520e728e7a40587bb09e98268ac401fb4d
Miguel Collado <m.collado.gomez@gmail.com>
2021-06-14 09:57:58 +0200

    Actualizar settings.py

--- settings.py
+++ settings.py
@@ -4,41 +4,6 @@ import os
 API_KEY = os.environ.get('BINANCE_API_KEY')
 API_SECRET = os.environ.get('BINANCE_API_SECRET')
 
-"""
-Crypto: Percentage
-"""
-
-percentage = 100 / 3
-
-# leaving a tiny piece of fiat untouched
-exposure = 0.5
-# exposure = 0.995  # Recommended max 0.995, min 0.005
-# exposure = 0.005
-
-portfolio_setting = {
-    # 'ADA': percentage * exposure,
-    'BTC': percentage * exposure,
-    'ETH': percentage * exposure,
-    # 'UNI': percentage * exposure,
-    # 'VET': percentage * exposure,
-    # 'DOT': percentage * exposure,
-    # 'SOL': percentage * exposure,
-    # 'AAVE': percentage * exposure,
-    'BNB': percentage * exposure,
-    # 'NANO': percentage * exposure,
-    # 'MATIC': percentage * exposure,
-    # 'XLM': percentage * exposure,
-    # 'EOS': percentage * exposure,
-    # 'LINK': percentage * exposure,
-    # 'ATOM': percentage * exposure,
-}
-
-
-# Percentage of deviation to rebalance. Set to None if want to disable it
-# for 1.0 value if asset has 10%, causes rebalancing if it takes >=11% or <=9%
-# minimum_percentage_deviation = 1.0 * exposure
-minimum_percentage_deviation = None
-
 
 """
 Fiat asset selected. Use BUSD, USDT, and so on
@@ -46,3 +11,40 @@ Fiat asset selected. Use BUSD, USDT, and so on
 
 fiat_asset = 'BUSD'
 fiat_decimals = 2
+
+"""
+Crypto: Percentage
+"""
+
+assets = [
+    'BTC',
+    'ETH',
+    'BNB',
+    # 'ADA',
+    # 'UNI',
+    # 'VET',
+    # 'DOT',
+    # 'SOL',
+    # 'AAVE',
+    # 'NANO',
+    # 'MATIC',
+    # 'XLM',
+    # 'EOS',
+    # 'LINK',
+    # 'ATOM',
+]
+
+# from all of your balance, control how much you are exposed with this setting. max 0.995, min 0.005
+# 0.995 means all of your balance will be used for assets rebalancing
+# 0.005 means nothing of your balance used.
+exposure = 0.5
+
+# automatically compute percentage for the number of assets selected, and use exposure to modify it.
+percentage = 100 / len(assets)
+portfolio_setting = {asset: percentage * exposure for asset in assets}
+
+
+# Percentage of deviation to rebalance. Set to None if want to disable it
+# for 1.0 value if asset has 10%, causes rebalancing if it takes >=11% or <=9%
+# minimum_percentage_deviation = 1.0 * exposure
+minimum_percentage_deviation = None
