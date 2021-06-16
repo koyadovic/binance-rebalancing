cac4529bdcb0ef5adcccfc321ea0a7133bc2b617
Miguel Collado <m.collado.gomez@gmail.com>
2021-06-16 13:29:34 +0200

    Actualizar settings.py

--- settings.py
+++ settings.py
@@ -1,16 +1,7 @@
 from core.domain.distribution import Distribution, EqualDistribution
 
-"""
-Fiat asset selected. Use BUSD, USDT, and so on
-"""
-
 fiat_asset = 'BUSD'
 fiat_decimals = 2
-
-"""
-Crypto assets
-"""
-
 crypto_assets = [
     'BTC',
     'ETH',
@@ -28,29 +19,5 @@ crypto_assets = [
     # 'LINK',
     # 'ATOM',
 ]
-
-"""
-from all of your balance, control how much you are exposed with exposure setting. max 0.995, min 0.005
-0.995 means all of your balance will be used for assets rebalancing
-0.005 means nothing of your balance used.
-
-with 0.5 you only expose the half of your balance.
-If every crypto asset goes down, automatic buy will occur.
-If every crypto asset goes up, automatic sell will occur.
-Always keeping the proportions.
-
-Example:
-fiat_asset = 'BUSD'
-fiat_decimals = 2
-crypto_assets = ["BTC", "ETH"]
-exposure = 0.5
-
-This will keep for every execution:
-- 50% in BUSD
-- 25% in BTC
-- 25% in ETH 
-"""
 exposure = 0.995
-
-# the other possibility is CustomDistribution
 distribution: Distribution = EqualDistribution(crypto_assets=crypto_assets)
