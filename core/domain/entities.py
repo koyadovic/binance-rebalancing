72b612df7a52e21b3b44c0e1e3be8c6e9741a88a
Miguel Collado <m.collado.gomez@gmail.com>
2021-06-24 08:54:19 +0200

    Actualizar entities.py

--- core/domain/entities.py
+++ core/domain/entities.py
@@ -1,16 +1,16 @@
 class Operation:
     pair: str
     type: str
-    quote_amount: float
+    counter_amount: float
 
     TYPE_BUY = 'BUY'
     TYPE_SELL = 'SELL'
     TYPES = [TYPE_SELL, TYPE_BUY]
 
-    def __init__(self, pair=None, type=None, quote_amount=None):
+    def __init__(self, pair=None, type=None, counter_amount=None):
         self.pair = pair.strip().upper()
         self.type = type
-        self.quote_amount = quote_amount
+        self.counter_amount = counter_amount
         self._self_validation()
 
     @property
@@ -18,19 +18,19 @@ class Operation:
         return self.pair.split('/')[0]
 
     @property
-    def quote_currency(self):
+    def counter_currency(self):
         return self.pair.split('/')[1]
 
     def _self_validation(self):
         if '/' not in str(self.pair):
-            raise ValueError(f'Invalid pair format. Must be provided as \"BASE/QUOTE\". For example: \"BTC/BUSD\"')
+            raise ValueError(f'Invalid pair format. Must be provided as \"BASE/COUNTER\". For example: \"BTC/BUSD\"')
         if self.type not in Operation.TYPES:
             raise ValueError(f'Invalid operation type. Accepted ones are: {", ".join(Operation.TYPES)}')
-        if self.quote_amount is None or self.quote_amount < 0:
+        if self.counter_amount is None or self.counter_amount < 0:
             raise ValueError(f'Invalid operation amount. Only positive values are allowed')
 
     def __str__(self):
-        return f'{self.type} {self.quote_currency}{self.quote_amount} of {self.base_currency}'
+        return f'{self.type} {self.counter_currency}{self.counter_amount} of {self.base_currency}'
 
     def __repr__(self):
         return self.__str__()
